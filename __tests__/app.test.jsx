2594e13c1a7c5175318daee9cf6b4ac1f1eb50ed
Enric Soler <enric.soler@bigfinite.com>
2017-10-29 14:14:51 +0100

    Add tests

--- __tests__/app.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { mount } from 'enzyme';
-// import App from '../src/app';
-
-test('welcomes the user to React', () => {
-  // const wrapper = mount(<App />);
-
-  // expect(wrapper.text()).toContain('Welcome to React');
-});
